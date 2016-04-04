f3ecba6276a5bab422f1fb00bea2d107ffa5d3a5
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2016-04-03 20:30:00 -0700

    Remove receipt code

--- js/common.js
+++ js/common.js
@@ -8,10 +8,6 @@ window.addEventListener("load", function common_onready() {
     window.applicationCache.update();
   } catch (ex) {}
 
-  /* Disable receipt checking while the app is free and receipts are broken on Android
-  mozmarket.receipts.verify(verifyReceipt);
-   */
-
   // setup the game
   Snake.setup();
 
@@ -55,33 +51,6 @@ function install() {
     // whoops - this.error.name has details
     console.log(this.error);
   };
-}
-
-function verifyReceipt(verifier) {
-  if (verifier.state instanceof verifier.states.OK
-      || window.location.hash == "#purchased") {
-    return;
-  }
-  if (verifier.state instanceof verifier.states.NeedsInstall) {
-    console.log('Install needed: ' + verifier.error);
-    $('body').addClass('purchaseNow');
-  } else if (verifier.state instanceof verifier.states.InternalError) {
-    // The verifier library itself got messed up; this shouldn't happen!
-    // It's up to you if you want to reject the user at this point
-    console.log('Internal error verifying app purchase: ' + verifier.error);
-  } else if (verifier.state instanceof verifier.states.NetworkError) {
-    // it was some kind of network or server error
-    // i.e., not the fault of the user
-    // you may want to let the user in, but for a limited time
-    console.log('Network error while verifying app purchase. Will try again later.');
-  } else {
-    // Some other error occurred; maybe it was never a valid receipt, maybe
-    // the receipt is corrupted, or someone is trying to mess around.
-    // It would not be a bad idea to log this.
-    if (verifier.app)
-        console.log('Unknown error: ' + verifier.app.receipts + ' ' + verifier.error);
-    $('body').addClass('purchaseNow');
-  }
 }
 
 function updateTweetButton() {
